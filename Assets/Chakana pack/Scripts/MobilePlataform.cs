60c0cc54a5fa20300059d40f22f1413ebc40fd70
Jhon Chalco <esteban01212@gmail.com>
2023-04-25 21:27:48 -0500

    jchalco: 'mejoras del nivel 6 y logica de "puertas elementales"'

--- Assets/Chakana pack/Scripts/MobilePlataform.cs
+++ Assets/Chakana pack/Scripts/MobilePlataform.cs
@@ -4,29 +4,37 @@ using UnityEngine;
 
 public class MobilePlataform : MonoBehaviour
 {
-    [SerializeField] private Transform sP, eP;
+    [SerializeField] private List<Transform> mP = new List<Transform>();
     [SerializeField] private float velocity;
 
-    private Vector3 destino;
+    private Vector3 destination;
+    private int lastPoint;
+    private int simbol = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        destino = eP.position;
+        destination = mP[0].position;
+        lastPoint = 0;
     }
 
     private void LateUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, destino, velocity * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, destination, velocity * Time.deltaTime);
 
-        if(transform.position == sP.position)
+        CheckMP();
+    }
+
+    private void CheckMP()
+    {
+        if (transform.position == mP[lastPoint].position)
         {
-            destino = eP.position;
-        }
-        
-        if(transform.position == eP.position)
-        {
-            destino = sP.position;
+            if (lastPoint == (mP.Count - 1)) simbol = -1;
+            else if (lastPoint == 0) simbol = 1;
+
+            lastPoint += simbol;
+
+            destination = mP[lastPoint].position;
         }
     }
 
