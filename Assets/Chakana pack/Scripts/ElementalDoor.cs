b6da3d42f0049a5e58294409c98b1983ea9f77ef
Jhon Chalco <esteban01212@gmail.com>
2023-04-27 19:16:12 -0500

    jchalco: 'correcciones de camara y nivel 8'

--- Assets/Chakana pack/Scripts/ElementalDoor.cs
+++ Assets/Chakana pack/Scripts/ElementalDoor.cs
@@ -40,7 +40,7 @@ public class ElementalDoor : MonoBehaviour
     {
         if (isMove)
         {
-            transform.position = Vector3.MoveTowards(transform.position, destination, 7 * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, destination, 5 * Time.deltaTime);
 
             if (transform.position == destination) isMove = false;
         }
