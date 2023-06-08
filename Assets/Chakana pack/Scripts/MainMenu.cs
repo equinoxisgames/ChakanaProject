703824990050097957b015439754adcb0f11d004
equinoxisgames <equinoxisgames@gmail.com>
2023-06-08 11:43:43 -0500

    Version estable main menú incluido cascada, pruebas realizadas con build

--- Assets/Chakana pack/Scripts/MainMenu.cs
+++ Assets/Chakana pack/Scripts/MainMenu.cs
@@ -39,12 +39,12 @@ public class MainMenu : MonoBehaviour
         btContinue.Select();
         Time.timeScale = 1f;
 
-        if (Input.GetJoystickNames().Length > 0 && !string.IsNullOrEmpty(Input.GetJoystickNames()[0]))
-        {
-            gamePadConectado = true;
-            joystickIzquierdoMovido = true;
-            //Debug.Log("Start Gamepad conectado");
-        }
+        //if (Input.GetJoystickNames().Length > 0 && !string.IsNullOrEmpty(Input.GetJoystickNames()[0]))
+        //{
+        //    gamePadConectado = true;
+        //    joystickIzquierdoMovido = true;
+        //    //Debug.Log("Start Gamepad conectado");
+        //}
         mouseMovido = true;
 
     }
