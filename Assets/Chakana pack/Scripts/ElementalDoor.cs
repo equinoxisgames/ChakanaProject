c74c124284a9b8996381bef89f70abbce15ab4d3
Jhon Chalco <esteban01212@gmail.com>
2023-07-12 18:17:25 -0500

    jchalco: 'Asignacion de prefabs y bug de puerta'

--- Assets/Chakana pack/Scripts/ElementalDoor.cs
+++ Assets/Chakana pack/Scripts/ElementalDoor.cs
@@ -29,11 +29,11 @@ public class ElementalDoor : MonoBehaviour
         else if (PlayerPrefs.GetInt(doorName) == 3)
         {
             PlayerPrefs.SetInt(doorName, 4);
-            Destroy(transform.parent);
+            Destroy(transform.parent.gameObject);
         }
         else
         {
-            Destroy(transform.parent);
+            Destroy(transform.parent.gameObject);
         }
     }
 
