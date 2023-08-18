0db35c687b559a2cfc982e1ffb2045f34e0ac9b5
Jhon Chalco <esteban01212@gmail.com>
2023-08-17 20:27:47 -0500

    jchalco: 'animaciones habilidades v2'

--- Assets/Chakana pack/Scripts/Hoyustus.cs
+++ Assets/Chakana pack/Scripts/Hoyustus.cs
@@ -561,10 +561,10 @@ public class Hoyustus : CharactersBehaviour
 
         yield return new WaitForSeconds(0.05f);
         anim.SetInteger("Skill", 0);
-        yield return new WaitForSeconds(0.15f);
+        yield return new WaitForSeconds(0.35f);
         Destroy(Instantiate(skillObj02, transform.position, Quaternion.identity), 1.2f);
         //SE ESPERA HASTA QUE SE GENERE ESTA EXPLOSION
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.8f);
         //SE VUELVEN A ESTABLECER LOS VALORES DE JUEGO NORMAL
         playable = true;
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
@@ -579,18 +579,18 @@ public class Hoyustus : CharactersBehaviour
         dashAvailable = false;
         cargaHabilidadSerpiente = 0f;
         cargaCuracion += 30;
-
+        yield return new WaitForSeconds(0.05f);
+        anim.SetInteger("Skill", 0);
+        yield return new WaitForSeconds(0.25f);
         //SE GENERA OTRO OBJETO A PARTIR DEL PREFAB BOLAVENENO Y SE LO MODIFICA
         GameObject bolaVenenoGenerada = Instantiate(bolaVeneno, transform.position + Vector3.up, Quaternion.identity);
         yield return new WaitForEndOfFrame();
         bolaVenenoGenerada.GetComponent<BolaVeneno>().aniadirFuerza(-transform.localScale.x, 11);
         yield return new WaitForEndOfFrame();
-        anim.SetInteger("Skill", 0);
         //SE VUELVEN A ESTABLECER LOS VALORES DE JUEGO NORMAL
         dashAvailable = true;
         playable = true;
         vidaMax = 1000;
-
     }
 
 
@@ -603,7 +603,6 @@ public class Hoyustus : CharactersBehaviour
         EstablecerInvulnerabilidades(layerObject);
         realizandoHabilidadLanza = true;
         playable = false;
-        Destroy(Instantiate(skillObj01, transform.position, Quaternion.identity, transform), 1.2f);
 
         invulnerable = true;
         cargaCuracion += 30;
@@ -615,6 +614,9 @@ public class Hoyustus : CharactersBehaviour
         rb.velocity = Vector2.zero;
         rb.gravityScale = 0f;
 
+        yield return new WaitForSeconds(0.05f);
+        anim.SetInteger("Skill", 0);
+        Destroy(Instantiate(skillObj01, transform.position, Quaternion.identity, transform), 1f);
         //ACTIVACION Y MODIFICACION DE LA LANZA
         lanzas[0].tag = "Fuego";
         ataque = valorAtaqueHabilidadLanza;
@@ -624,8 +626,8 @@ public class Hoyustus : CharactersBehaviour
 
         IEnumerator movimientoHabilidadLanza()
         {
-            rb.AddForce(new Vector2(transform.localScale.x * 20, 0), ForceMode2D.Impulse);
-            yield return new WaitForSeconds(1);
+            rb.AddForce(new Vector2(transform.localScale.x * 30, 0), ForceMode2D.Impulse);
+            yield return new WaitForSeconds(0.5f);
             rb.velocity = Vector2.zero;
             realizandoHabilidadLanza = false;
             //isDashing = false;
