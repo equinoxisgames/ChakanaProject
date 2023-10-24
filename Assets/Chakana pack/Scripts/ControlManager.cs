6a33fea9743d40a6c26bec395b6423420a63a3aa
equinoxisgames <equinoxisgames@gmail.com>
2023-10-24 01:41:34 -0500

    Ajustes UI usar gamepad

--- Assets/Chakana pack/Scripts/ControlManager.cs
+++ Assets/Chakana pack/Scripts/ControlManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//using UnityEngine.UI;
+
 public class ControlManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> controlParent = new List<GameObject>();
@@ -10,6 +12,10 @@ public class ControlManager : MonoBehaviour
     private List<GameObject> joyObj = new List<GameObject>();
     private bool joystick;
 
+    //public Slider sliderMaster;
+
+    Vector3 ultimaPosicionDelMouse;
+
     void Start()
     {
         for(int i = 0; i < controlParent.Count; i++)
@@ -17,16 +23,35 @@ public class ControlManager : MonoBehaviour
             keyObj.Add(controlParent[i].transform.GetChild(0).gameObject);
             joyObj.Add(controlParent[i].transform.GetChild(1).gameObject);
         }
+
+        ultimaPosicionDelMouse = Input.mousePosition;
     }
 
     void Update()
     {
+        Vector3 posicionActualDelMouse = Input.mousePosition;
+
+        if (posicionActualDelMouse != ultimaPosicionDelMouse)
+        {
+            // El mouse se ha movido
+            Debug.Log("El mouse se ha movido");
+            ultimaPosicionDelMouse = posicionActualDelMouse;
+
+            if (joystick)
+            {
+                joystick = false;
+                Cursor.visible = true;
+
+                ChangeControls(true);
+            }
+        }
+
         if (Input.anyKeyDown)
         {
             if (joystick)
             {
                 joystick = false;
-
+                Cursor.visible = true;
                 ChangeControls(true);
             }
         }
@@ -36,9 +61,11 @@ public class ControlManager : MonoBehaviour
             if (!joystick)
             {
                 joystick = true;
-
+                //sliderMaster.Select();
+                Cursor.visible = false;
                 ChangeControls(false);
-            }
+                
+}
         }
     }
 
