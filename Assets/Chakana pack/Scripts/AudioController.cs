c2c7c9c8ad71b693f99d355fa2213cdf2304bccb
equinoxisgames <equinoxisgames@gmail.com>
2023-10-26 16:19:06 -0500

    Audio Control Select
    Cambios en los objetos de audio control para que los sliders queden seleccionados al cambiarlos con el mouse, adicionalmente los nuevos sprites de los amuletos.

--- Assets/Chakana pack/Scripts/AudioController.cs
+++ Assets/Chakana pack/Scripts/AudioController.cs
@@ -2,24 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioController : MonoBehaviour
 {
     [SerializeField] private AudioMixer soundAudioMixer;
+    public Slider sliderSelected;
 
 
     public void MasterController(float masterSlider)
     {
         soundAudioMixer.SetFloat("MasterVolume", Mathf.Log10(masterSlider) * 20);
+        sliderSelected.Select();
     }
 
     public void MusicController(float musicSlider)
     {
         soundAudioMixer.SetFloat("MusicVolume", Mathf.Log10(musicSlider) * 20);
+        sliderSelected.Select();
     }
 
     public void SFXController(float SFXSlider)
     {
         soundAudioMixer.SetFloat("SFXVolume", Mathf.Log10(SFXSlider) * 20);
+        sliderSelected.Select();
     }
 }
