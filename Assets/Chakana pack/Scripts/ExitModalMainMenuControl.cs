bb3d0da42dafc4d0b46f734b7bbe405852053109
equinoxisgames <equinoxisgames@gmail.com>
2024-01-10 12:47:40 -0500

    Correcciones en UI Main Menu

---
+++ Assets/Chakana pack/Scripts/ExitModalMainMenuControl.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using Assets.FantasyInventory.Scripts.Interface.Elements;
+
+public class ExitModalMainMenuControl : MonoBehaviour
+{
+    //variables
+    public Button   btContinueExitGame;
+    public bool     activateBtContinueExitGame = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Validar movimiento del mouse
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            activateBtContinueExitGame = true;
+        }
+
+        Gamepad gamepad = Gamepad.current;
+        if (gamepad != null)
+        {
+            Vector2 leftStick = gamepad.leftStick.ReadValue();
+
+            if (leftStick != Vector2.zero && !Input.GetKey(KeyCode.UpArrow) && !Input.GetKey(KeyCode.DownArrow) &&
+                !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("El joystick izquierdo del gamepad se ha movido ");
+                if (btContinueExitGame != null && btContinueExitGame.IsInteractable() && btContinueExitGame.gameObject == UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject)
+                {
+                    Debug.Log("El botón está seleccionado y habilitado.");
+                    // Realiza aquí las acciones correspondientes cuando el botón está seleccionado y habilitado.
+                }
+                else
+                {
+                    Debug.Log("El botón no está seleccionado y/o no está habilitado.");
+                    // Realiza aquí las acciones correspondientes cuando el botón no está seleccionado o no está habilitado.
+                    if (activateBtContinueExitGame)
+                    {
+                        btContinueExitGame.Select();
+                        activateBtContinueExitGame = false;
+                    }
+                }
+            }
+        }
+    }
+}
